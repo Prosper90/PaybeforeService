0be4712ce14ec5b74c4298968abc1ecf8292a6ff
Prosper <nkonyeprospero@gmail.com>
2023-12-21 01:40:00 +0100

    all done

--- controllers/referrals.controllers.js
+++ controllers/referrals.controllers.js
@@ -9,9 +9,39 @@ const { Bonus } = require("../models/Bonus.js");
  */
 exports.GetAllRefs = async (req, res, next) => {
   try {
-    const refs = await Bonus.find({ owner: req.user._id });
-    if (!refs) return next(new ErrorResponse("No ref found"), 201);
-    res.status(200).json({ status: true, data: refs });
+    // Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const totalDocuments = await Bonus.countDocuments({
+      owner: req.user._id,
+    });
+    // const refs = await Bonus.find({ owner: req.user._id });
+    const refs = await Bonus.find({ owner: req.user._id })
+      .skip(startIndex)
+      .limit(limit);
+
+    if (!refs || refs.length === 0) {
+      return next(new ErrorResponse("No transactions found"), 201);
+    }
+    const pagination = {};
+
+    if (endIndex < totalDocuments) {
+      pagination.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    res.status(200).json({ status: true, data: refs, pagination });
   } catch (error) {
     next(error);
   }
